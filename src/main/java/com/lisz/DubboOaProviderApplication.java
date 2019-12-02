bad7f3f905d4e915423d32d7d593fe80407cbb84
lisz1012 <lisz1012@163.com>
2019-12-01 21:55:22 -0800

    Add notes

--- src/main/java/com/lisz/DubboOaProviderApplication.java
+++ src/main/java/com/lisz/DubboOaProviderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //防止去找datasource
 public class DubboOaProviderApplication {
 
 	public static void main(String[] args) {
