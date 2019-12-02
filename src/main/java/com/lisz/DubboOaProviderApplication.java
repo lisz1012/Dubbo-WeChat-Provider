fb3d53c6fa384333e8029fc6ea93e71b8ebed65e
lisz1012 <lisz1012@163.com>
2019-12-01 21:47:10 -0800

    Init

--- src/main/java/com/lisz/DubboOaProviderApplication.java
+++ src/main/java/com/lisz/DubboOaProviderApplication.java
@@ -2,8 +2,9 @@ package com.lisz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DubboOaProviderApplication {
 
 	public static void main(String[] args) {
